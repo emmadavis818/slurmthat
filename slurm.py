86133c156c762b5303d91eb3ef63eee61e23507f
Maurice <mauriceabraham38@gmail.com>
2026-06-24 11:26:03 -0700

    Wrote docstring for write function

--- slurm.py
+++ slurm.py
@@ -16,6 +16,24 @@ class job:
 
     def write(self, fail=False, start=False, finish=False, out_file=None, 
               err_file=None, time=32, cpus=10, nodes=1, memory=48):
+      '''Write
+      
+      Write a .sh file for slurm submission.
+        
+      Args:
+          fail(bool): Boolean. If True, sends email on job failure.
+          start(bool): Boolean. If True, sends email when job starts.
+          finish(bool): Boolean. If True, sends email when job finishes.
+          out_file(str): String. Name of the output file. 
+          err_file(str): String. Name of the error file. 
+          time(int): Integer. Time (in hours) requested for the job. Default is 32 hours.
+          cpus(int): Integer. Number of CPUs requested for the job. Default is 10.
+          nodes(int): Integer. Number of nodes requested for the job. Default is 1.
+          memory(int): Integer. Amount of memory (in GB) requested for the job. Default is 48 GB.
+      
+      Returns:
+          str: The name of the .sh file written.
+      '''
       sh_filename = self.filename
 
       with open(sh_filename, 'w') as f:
